0dbbc33ae701b1c4b235fd0e68cb7c9052d60bab
Adrian <alextravc.342@gmail.com>
2023-12-25 11:23:44 -0500

    [ADD]: ProjectSerializer

---
+++ projects/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+# ### Serializers based on Existing Models
+from .models import Project
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        # fields q podran ser consultados
+        fileds = ('id', 'title', 'description', 'technology', 'created_at')
+        # fields q son solo de lectura para evitar su manipulacion
+        read_only_fields = ('created_at')
+
+
